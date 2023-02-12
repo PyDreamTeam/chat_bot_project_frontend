18891a09df22acd32614373437174671227b3fb8
Shumigai Natalia <shumigai_frontend@mail.ru>
2023-02-12 09:09:18 +0300

    feat: add header

---
+++ src/components/common/Logo.styled.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+type logoProps = {
+    // прописать цвет для header или footer
+}
+
+export const Logo = styled.div <logoProps>`
+  font-family: 'Inter', sans-serif;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 40px;
+  line-height: 48px;
+  color: #17181A;
+`
