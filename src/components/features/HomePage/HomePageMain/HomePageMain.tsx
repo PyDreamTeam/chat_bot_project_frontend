4ed7a4e737246a6cf7af42e0f1fa87f6977c1353
fomkinwork <fomkinwork@gmail.com>
2023-03-04 22:54:32 +0300

    add template selections block

--- src/components/features/HomePage/HomePageMain/HomePageMain.tsx
+++ src/components/features/HomePage/HomePageMain/HomePageMain.tsx
@@ -4,6 +4,7 @@ import styles from "./HomePageMain.module.css";
 import HomePageHeader from "@/src/components/features/HomePage/HomePageHeader/HomePageHeader";
 import InputSearchField from "@/src/components/shared/inputs/InputSearchField";
 import Solutions from "@/src/components/entities/solutions/Solutions";
+import TemplateSelections from "@/src/components/entities/templateselections/TemplateSelections";
 
 const HomePageMain = () => {
      return (
@@ -11,6 +12,7 @@ const HomePageMain = () => {
                <HomePageHeader />
                <InputSearchField />
                <Solutions />
+               <TemplateSelections/>
           </main>
      );
 };
