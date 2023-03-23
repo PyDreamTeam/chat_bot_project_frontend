53eb5b2b9ab89f14fdfca8ba8df4dc74a2ee71e2
Shumigai Natalia <shumigai_frontend@mail.ru>
2023-03-23 17:01:12 +0300

    Fix Component AboutSettingsChatbot

---
+++ src/components/shared/listIcon/ListIcon.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CheckMark from "@/src/components/shared/checkMark/CheckMark";
+
+const ListIcon = () => {
+     return (
+          <div>
+               <CheckMark/>
+          </div>
+     );
+};
+
+export default ListIcon;
