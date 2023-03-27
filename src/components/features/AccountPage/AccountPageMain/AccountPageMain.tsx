ca3d6ca44c05f039e200a19ce1cf4b7b5606153e
fomkinwork <fomkinwork@gmail.com>
2023-03-28 00:08:49 +0300

    fix commit

--- src/components/features/AccountPage/AccountPageMain/AccountPageMain.tsx
+++ src/components/features/AccountPage/AccountPageMain/AccountPageMain.tsx
@@ -6,7 +6,6 @@ import TemplateSelections from "@/src/components/entities/templateselections/Tem
 import SearchHistory from "@/src/components/entities/SearchHistory/SearchHistory";
 import { useAppSelector } from "@/src/hooks/types";
 import SelectInAccount from "@/src/components/entities/selectInAccount/selectInAccount";
-import Rates from "@/src/components/entities/rates/Rates";
 
 const AccountPageMain = () => {
      const { name } = useAppSelector((state) => state.credentialsSlice.credentials);
@@ -15,28 +14,9 @@ const AccountPageMain = () => {
      //      console.log(name);
      // }, [name]);
 
-     const RatesConfig = [
-          {
-               title: "Basic",
-               price: 19000,
-               descriptions: ["Описание тарифа, его особенности", "Описание тарифа, его особенности", "Описание тарифа, его особенности"]
-          },
-          {
-               title: "Basic",
-               price: 19000,
-               descriptions: ["Описание тарифа, его особенности", "Описание тарифа, его особенности", "Описание тарифа, его особенности"]
-          },
-          {
-               title: "Basic",
-               price: 19000,
-               descriptions: ["Описание тарифа, его особенности", "Описание тарифа, его особенности", "Описание тарифа, его особенности"]
-          }
-     ];
-
      return (
           <main>
                <InputSearchField />
-               <Rates cards={RatesConfig}/>
                <SelectInAccount />
                <Solutions />
                <TemplateSelections />
--- src/pages/my-account/[slug]/changepassword.tsx
+++ src/pages/my-account/[slug]/changepassword.tsx
@@ -3,7 +3,6 @@ import AccountPageWrapper from "@/src/components/widgets/AccountpageWrapper";
 import AccountPageCredential from "@/src/components/entities/accountpageSettings/AccountpageCredentials";
 import { useAppSelector } from "@/src/hooks/types";
 import FormMyAccount from "@/src/components/entities/forms/formMyAccount/FormMyAccount";
-import ErrorList from "@/src/components/entities/errorList/ErrorList";
 
 const SettingsPage = () => {
      const { id, name, email } = useAppSelector((state) => state.credentialsSlice.credentials);
@@ -13,7 +12,6 @@ const SettingsPage = () => {
           <AccountPageWrapper title={"Настройки"}>
                <AccountPageCredential id={id} mobileNumber={mobileNumber} email={email} isEmailVerified={true} name={name} />
                <FormMyAccount type={"changePassword"} />
-               <ErrorList />
           </AccountPageWrapper>
      );
 };
