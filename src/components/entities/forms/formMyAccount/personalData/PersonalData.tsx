bf3eab8a1fd436f3290b68dd5c005321824e1717
PseudoElement <bubamainer@mail.ru>
2023-03-30 22:43:37 +0300

    fix names

--- src/components/entities/forms/formMyAccount/personalData/PersonalData.tsx
+++ src/components/entities/forms/formMyAccount/personalData/PersonalData.tsx
@@ -4,11 +4,12 @@ import FormUniversal from "@/src/components/entities/forms/FormUniversal";
 import styles from "../styles/FormMyAccount.module.css";
 import TextField from "@/src/components/shared/textfields/TextField";
 
-
-const PersonalData = () => {
+const PersonalDataForm = () => {
      return (
           <div className={styles.personalDataBlock}>
-               <TextField color={"black"} type={"h4"}>Персональные данные</TextField>
+               <TextField color={"black"} type={"h4"}>
+                    Персональные данные
+               </TextField>
                <FormUniversal
                     onSubmit={() => console.log(1)}
                     validationSchema={validationSchemaPersonData}
@@ -21,4 +22,4 @@ const PersonalData = () => {
      );
 };
 
-export default PersonalData;
+export default PersonalDataForm;
--- src/pages/my-account/profile/personal-data.tsx
+++ src/pages/my-account/profile/personal-data.tsx
@@ -6,9 +6,9 @@ import SearchHistory from "@/src/components/entities/SearchHistory/SearchHistory
 import AccountPageCredential from "@/src/components/entities/accountpageSettings/AccountpageCredentials";
 import SelectTitle from "@/src/components/entities/selectTitle/SelectTitle";
 import { SELECT_TITLE_CONFIG } from "@/src/components/entities/selectTitle/SelectTitleConfig";
-import PersonalData from "@/src/components/entities/forms/formMyAccount/personalData/PersonalData";
+import PersonalDataForm from "@/src/components/entities/forms/formMyAccount/personalData/PersonalData";
 
-const ProfilePage = () => {
+const PersonalData = () => {
      const router = useRouter();
      const { name, email } = useAppSelector((state) => state.credentialsSlice.credentials);
      const mobileNumber = "+375297177707";
@@ -22,9 +22,9 @@ const ProfilePage = () => {
                     isEmailVerified={true}
                     name={name}
                />
-               <PersonalData />
+               <PersonalDataForm />
           </AccountPageWrapper>
      );
 };
 
-export default ProfilePage;
+export default PersonalData;
