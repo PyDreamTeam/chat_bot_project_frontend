dd84b53e52094cda58d8f3dd1a79678a85f3cbda
PseudoElement <bubamainer@mail.ru>
2023-04-08 15:48:59 +0300

    add test action

--- src/shared/enums/my-account.ts
+++ src/shared/enums/my-account.ts
@@ -5,4 +5,5 @@ export const AccountPageTypes = {
      profile_templates: "profile_templates",
      profile_settings_personalData: "profile_settings_personalData",
      profile_settings_password: "profile_settings_password",
+     profile_changeData: "profile_changeData",
 } as const;
