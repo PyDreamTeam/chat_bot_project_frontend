81156bc6653784ed49e1250723524998fcff16b5
antonsoloviev <led_soloviev@mail.ru>
2023-07-11 19:46:50 +0300

    added modal window SelectionRequest

--- src/components/shared/buttons/Button.tsx
+++ src/components/shared/buttons/Button.tsx
@@ -1,14 +1,18 @@
-import {FC, PropsWithChildren} from "react";
+import { FC, PropsWithChildren } from "react";
 import css from "./styles/button.module.css";
 
 type StyleButton = "button";
 type TypeButton = "submit" | "button" | "reset";
 
 interface PropsButton {
-    style: StyleButton
-    type: TypeButton
-    disabled?: boolean
+     style: StyleButton;
+     type: TypeButton;
+     disabled?: boolean;
+     onClick?: () => void;
 }
 
-export const Button: FC<PropsWithChildren<PropsButton>> = ({disabled, children, style, type}) => 
-     <button disabled={disabled} className={css[style]} type={type}>{children}</button>;
+export const Button: FC<PropsWithChildren<PropsButton>> = ({ disabled, children, style, type, onClick }) => (
+     <button disabled={disabled} className={css[style]} type={type} onClick={onClick}>
+          {children}
+     </button>
+);
