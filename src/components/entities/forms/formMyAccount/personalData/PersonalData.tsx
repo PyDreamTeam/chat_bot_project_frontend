88b0140a0ff1f952d32ecc4bbeb99f25ff443b68
Alexander12081995 <sgen28@gmail.com>
2023-07-22 00:23:11 +0300

    fixed bugs

--- src/components/entities/forms/formMyAccount/personalData/PersonalData.tsx
+++ src/components/entities/forms/formMyAccount/personalData/PersonalData.tsx
@@ -1,45 +1,10 @@
 import React, {useEffect} from "react";
-import { initialValuesPersonData, inputPersonData, validationSchemaPersonData } from "@/src/pagesData/personal-data";
-import FormUniversal from "@/src/components/entities/forms/FormUniversal";
 import styles from "../styles/FormMyAccount.module.css";
 import Title from "@/src/components/shared/text/Title";
 import { DataForm } from "../../../DataForm/DataForm";
-import * as Yup from "yup";
 import { useVerifyUserMutation } from "@/src/store/services/userAuth";
 import { useRouter } from "next/router";
 import Cookies from "js-cookie";
-
-interface PersonalData {
-     htmlFor: string
-     label: string
-     type: string
-     name: "first_name" | "last_name" | "number";
-     placeholder: string
-}
-
-const schema: Array<PersonalData> = [
-     {
-          htmlFor: "first_name",
-          label: "Имя",
-          name: "first_name",
-          placeholder: "Иван",
-          type: "text"
-     },
-     {
-          htmlFor: "last_name",
-          label: "Фамилия",
-          name: "last_name",
-          placeholder: "Иванов",
-          type: "text"
-     },
-     {
-          htmlFor: "number",
-          label: "Телефон",
-          name: "number",
-          placeholder: "+375299652084",
-          type: "text"
-     }
-];
 
 const PersonalDataForm = () => {
      const token = JSON.parse(Cookies.get("loginUser") || "[]");
@@ -60,7 +25,7 @@ const PersonalDataForm = () => {
                <Title color={"black"} type={"h5"}>
                     Персональные данные
                </Title>
-               <DataForm schema={schema}/>
+               <DataForm/>
           </div>
      );
 };
