5db061af8af648f2a90a8701e18a1ac1ec0dc711
Alexander12081995 <sgen28@gmail.com>
2023-07-21 22:12:29 +0300

    fixed login

--- src/pages/change-password/index.tsx
+++ src/pages/change-password/index.tsx
@@ -1,16 +1,15 @@
 import Text from "@/src/components/shared/text/Text";
-import Title from "@/src/components/shared/text/Title";
-import { CreateUserResponse } from "@/src/types/user";
-import { ErrorMessage, Field, Form, Formik } from "formik";
-import Image from "next/image";
+import { Form, Formik } from "formik";
 import { useEffect, useState } from "react";
 import * as Yup from "yup";
 import css from "./changePassword.module.css";
-import { ErrorsPassword } from "@/src/components/entities/errorsPassword/ErrorsPassword";
 import { useRouter } from "next/router";
 import { useChangePasswordMutation, useDataUserQuery } from "@/src/store/services/userAuth";
 import Cookies from "js-cookie";
 import AuthWrapper from "@/src/components/wrappers/AuthWrapper";
+import { ChangeNewPasswordInput } from "@/src/components/shared/login/ChangeNewPasswordInput/ChangeNewPasswordInput";
+import { ReChangeNewPasswordInput } from "@/src/components/shared/login/ReChangeNewPasswordInput/ReChangeNewPasswordInput";
+import { ButtonLogin } from "@/src/components/shared/buttons/ButtonLogin";
 
 const ChangePassword = () => {
 
@@ -39,9 +38,15 @@ const ChangePassword = () => {
 
      const router = useRouter();
      const { uid, token } = router.query;
-     const [changePassword, {isSuccess}] = useChangePasswordMutation();
+     const [changePassword, {isSuccess, isLoading, error}] = useChangePasswordMutation();
      const tn = JSON.parse(Cookies.get("loginUser") || "[]");
      const {data} = useDataUserQuery(tn);
+
+     useEffect(() => {
+          if(isSuccess) {
+               router.push("/change-password/sign-in_new-password");
+          }
+     }, [isSuccess]);
      
 
      return (
@@ -66,17 +71,11 @@ const ChangePassword = () => {
                                    uid: Yup.string(),
                                    token: Yup.string()
                               })}
-                              onSubmit={(values, {setSubmitting}) => {
-                                   setTimeout(() => {
-                                        setSubmitting(false);
-                                   }, 1000);
+                              onSubmit={(values) => {
                                    changePassword({...values, uid, token});
-                                   if(isSuccess) {
-                                        router.push("/change-password/sign-in_new-password");
-                                   }
                               }}
                          >
-                              {({ isSubmitting, errors, touched, isValid, getFieldProps }) => {
+                              {({ errors, touched, isValid, getFieldProps }) => {
 
                                    const dataPassword = getFieldProps("new_password");
                                    const password = dataPassword.value;
@@ -87,58 +86,9 @@ const ChangePassword = () => {
                                                   <Text type="reg20" color="dark">{data?.first_name} {data?.last_name}</Text>
                                                   <Text type="reg20" color="dark">{data?.email}</Text>
                                              </div>
-                                             <div className={css.blockInput}>
-
-                                                  <label htmlFor="new_password">
-                                                       <Text type="reg18" color="dark">Новый пароль</Text>
-                                                  </label>
-
-                                                  <div className={css.errorIcon}>
-                                                       {errors.new_password && touched.new_password && <Image src="/sign/errorIcon.svg" width={24} height={24} alt="errorIcon" />}
-                                                  </div>
-
-                                                  <div className={css.groupStateEye}>
-                                                       <Field type={openPassword} name="new_password" placeholder="Введите новый пароль" className={errors.new_password && touched.new_password ? `${css.inputError}` : isValid && touched.new_password ? `${css.valid}` : `${css.input}`} />
-                                                       <div className={css.stateEye}>
-                                                            {openPassword === "password" && <Image src="/sign/closePassword.svg" width={24} height={24} alt="stateEye" onClick={viewPassword} />}
-                                                            {openPassword === "text" && <Image src="/sign/openPassword.svg" width={24} height={24} alt="stateEye" onClick={viewPassword} />}
-                                                       </div>
-                                                  </div>
-
-                                                  {touched.new_password &&
-                                                 <div className={css.errorsBlock}>
-                                                      <ErrorsPassword password={password}/>
-                                                 </div>}
-
-                                             </div>
-
-                                             <div className={css.blockInput}>
-                                                  <label htmlFor="re_new_password">
-                                                       <Text type="reg18" color="dark">Повторить пароль</Text>
-                                                  </label>
-
-                                                  <div className={css.errorIcon}>
-                                                       {errors.re_new_password && touched.re_new_password && <Image src="/sign/errorIcon.svg" width={24} height={24} alt="errorIcon" />}
-                                                  </div>
-
-                                                  <div className={css.groupStateEye}>
-                                                       <Field type={openConfirmPassword} name="re_new_password" placeholder="Повторите новый пароль" className={errors.re_new_password && touched.re_new_password ? `${css.inputError}` : isValid && touched.re_new_password ? `${css.valid}` : `${css.input}`} />
-                                                       <div className={css.stateEye}>
-                                                            {openConfirmPassword === "password" && <Image src="/sign/closePassword.svg" width={24} height={24} alt="stateEye" onClick={confirmPassword} />}
-                                                            {openConfirmPassword === "text" && <Image src="/sign/openPassword.svg" width={24} height={24} alt="stateEye" onClick={confirmPassword} />}
-                                                       </div>
-                                                  </div>
-
-                                                  <div className={css.error}>
-                                                       <Text type="reg16" color="red">
-                                                            <ErrorMessage name="re_new_password" />
-                                                       </Text>
-                                                  </div>
-                                             </div>
-
-                                             <button type="submit" disabled={isSubmitting} className={isValid ? `${css.button}` : `${css.buttonDisabled}`}>
-                                               Обновить пароль
-                                             </button>
+                                             <ChangeNewPasswordInput errors={errors} touched={touched} password={password} error={error}/>
+                                             <ReChangeNewPasswordInput errors={errors} touched={touched} error={error}/>
+                                             <ButtonLogin disabled={isLoading} active={isValid} type="submit">Обновить пароль</ButtonLogin>
                                         </Form>
                                    );
                               }}
--- src/pages/sign-in/index.tsx
+++ src/pages/sign-in/index.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import ComponentSignIn from "./componentSignIn/ComponentSignIn";
 import Text from "@/src/components/shared/text/Text";
 import Link from "next/link";
@@ -7,41 +6,7 @@ import AuthWrapper from "@/src/components/wrappers/AuthWrapper";
 import IconsAuthBar from "@/src/components/entities/iconbars/IconsAuthBar";
 import { svgArray } from "@/src/components/entities/iconbars/img/svgConfig";
 
-
-interface UserInfo {
-     htmlFor: string
-     label: string
-     type: string
-     name: "email" | "password";
-     placeholder: string
-}
-
-
-
-
 function SignIn() {
-
-     const [typePassword, setTypePassword] = useState<string>("password");
-
-     const open = () => setTypePassword("text");
-     const close = () => setTypePassword("password");
-
-     const schema: Array<UserInfo> = [
-          {
-               htmlFor: "email",
-               label: "E-mail",
-               type: "email",
-               name: "email",
-               placeholder: "example@mail.com"
-          },
-          {
-               htmlFor: "password",
-               label: "Пароль",
-               type: typePassword,
-               name: "password",
-               placeholder: "Введите пароль"
-          }
-     ];
 
      return (
           <div className={css.container}>
@@ -57,7 +22,7 @@ function SignIn() {
                          <div className={css.text}>
                               <Text type="reg16" color="grey">Или с помощью почты и пароля</Text>
                          </div>
-                         <ComponentSignIn schema={schema} open={open} close={close}/>
+                         <ComponentSignIn/>
                     </div>
                </AuthWrapper>
           </div>
