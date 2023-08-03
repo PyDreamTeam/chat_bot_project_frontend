945b0c438ab4900d2294b091d35e72d245e2d9e3
antonsoloviev <led_soloviev@mail.ru>
2023-08-03 08:38:16 +0300

    fix: change baseUrl to https

--- src/store/services/userAuth.ts
+++ src/store/services/userAuth.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const userAuth = createApi({
      reducerPath: "userAuth",
-     baseQuery: fetchBaseQuery({baseUrl: "http://python.twnsnd.online"}),
+     baseQuery: fetchBaseQuery({baseUrl: "https://python.twnsnd.online"}),
      endpoints: (builder) => ({
           dataUser: builder.query({
                query: (token: {access: string, refresh: string}) => ({
