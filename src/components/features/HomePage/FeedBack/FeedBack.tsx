332811a5e6e9465419c937267349b68f53aa894b
ViyaSilyantieva <viya985@gmail.com>
2023-08-18 18:04:41 +0300

    fix: fixed inconsistencies in layout

--- src/components/features/HomePage/FeedBack/FeedBack.tsx
+++ src/components/features/HomePage/FeedBack/FeedBack.tsx
@@ -4,10 +4,14 @@ import Text from "@/src/components/shared/text/Text";
 import { CARDS_FEEDBACK_ALL } from "@/src/components/entities/cards/cardsFeedback/CardsFeedbackAllConfig";
 import ListBigCardsFeedback from "@/src/components/entities/lists/listCardsFeedback/ListBigCardsFeedback";
 import styles from "./FeedBack.module.css";
+import Link from "next/link";
 
 const FeedBack = () => {
      return (
           <div className={styles.wrapper}> 
+               <div className={styles.links}>
+                    <Text type="reg14" color="telegray"><Link className={styles.link} href={"/home"}>Главная</Link>/<Link className={styles.link} href={"/home"}>Посмотреть все</Link></Text>
+               </div>
                <div className={styles.title}>
                     <Title type={"h3"} color={"dark"}>Отзывы</Title>
                     <Text type={"reg16"} color={"grey"}>Что говорят о нас наши клиенты</Text>
