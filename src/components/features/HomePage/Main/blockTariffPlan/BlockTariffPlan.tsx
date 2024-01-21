7282aff4823dcceef235e51be5a3cdf57c5efc3e
antonsoloviev <led_soloviev@mail.ru>
2024-01-21 21:46:29 +0300

    add tarifs querys

--- src/components/features/HomePage/Main/blockTariffPlan/BlockTariffPlan.tsx
+++ src/components/features/HomePage/Main/blockTariffPlan/BlockTariffPlan.tsx
@@ -1,47 +1,22 @@
 import { TariffCard } from "@/src/components/shared/TariffCard/TariffCard";
-import CheckMark from "@/src/components/shared/checkMark/CheckMark";
 import { TariffPlanCard } from "@/src/types";
 import css from "./blockTariffPlan.module.css";
 import Title from "@/src/components/shared/text/Title";
+import Cookies from "js-cookie";
+import { useGetTariffsListQuery } from "@/src/store/services/userAuth";
 
-const tariffPlansCards: TariffPlanCard[] = [
-    {
-        title: "Решение с доработкой",
-        price: "от 9 000 ₽",
-        icon: <CheckMark />,
-        advantage: ["1 решение", "Переводы и платежи", "Переводы и платежи", "Переводы и платежи", "Базовая аналитика"],
-        hotPlan: false,
-    },
-    {
-        title: "Готовое решение",
-        price: "от 17 000 ₽",
-        icon: <CheckMark />,
-        advantage: [
-            "5 решений",
-            "Переводы и платежи",
-            "Переводы и платежи",
-            "Переводы и платежи",
-            "Премиальная аналитика",
-        ],
-        hotPlan: true,
-        bestPlan: "/img/hotPlan.svg",
-    },
-    {
-        title: "Решение с нуля",
-        price: "от 29 000 ₽",
-        icon: <CheckMark />,
-        advantage: [
-            "8 решений",
-            "Переводы и платежи",
-            "Переводы и платежи",
-            "Переводы и платежи",
-            "Премиальная аналитика",
-        ],
-        hotPlan: false,
-    },
-];
+interface ITariff {
+    id?: number;
+    tags_of_rates: string[];
+    title: string;
+    price: string;
+    created_at?: string;
+    is_special: string | null;
+}
 
 export const BlockTariffPlan = () => {
+    const { data, isLoading, isSuccess } = useGetTariffsListQuery({});
+
     return (
         <div className={css.wrapper}>
             <div className={css.title}>
@@ -50,16 +25,13 @@ export const BlockTariffPlan = () => {
                 </Title>
             </div>
             <ul className={css.container}>
-                {tariffPlansCards.map((item, index) => (
+                {data?.results.map((item: ITariff) => (
                     <TariffCard
-                        key={index}
+                        key={item.id}
                         title={item.title}
                         price={item.price}
-                        icon={item.icon}
-                        advantage={item.advantage}
-                        hotPlan={item.hotPlan}
-                        bestPlan={item.bestPlan}
-                        dataComment={`Выбранный тариф: ${item?.title}`}
+                        advantages={item.tags_of_rates}
+                        hotPlan={item.is_special === "hot plan" ? true : false}
                     />
                 ))}
             </ul>
